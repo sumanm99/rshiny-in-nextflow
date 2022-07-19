f988abfd5b92e876b7862041ce15050c92a679d3
sumanm99 <sumanmuralidharan1999@gmail.com>
2022-07-19 17:12:56 +0530

    Update app.R

--- app.R
+++ app.R
@@ -10,32 +10,39 @@ tax_table <- read.csv(paste("./",args[1],sep=""), header=T)
 count_table <- read.csv(paste("./",args[2],sep=""), header=T)
 metadata <- read.csv(paste("./",args[3],sep=""), header=T)
 
-# tax_table <- read.csv("./tax_table.csv", header=T)
-# count_table <- read.csv("./count_table.csv", header=T)
-# metadata <- read.csv("./metadata.csv", header=T)
-
-
-ui <- fluidPage(
-  titlePanel("proBiome Suite"),
-  br(),
-  mainPanel(
-    dataTableOutput("table1"),
-    br(),
-    dataTableOutput("table2"),
-    br(),
-    dataTableOutput("table3")
+ui <- tagList(
+  titlePanel(
+    HTML('<p style="background-color: Black;
+                color: white;
+                padding-left: 15px;">proBiome Suite 
+                <span style="font-size: 10pt;">&#8862 Data</span></p>')
+  ),
+  fluidPage(
+    mainPanel(
+      dataTableOutput("table1"),
+      br(),
+      dataTableOutput("table2"),
+      br(),
+      dataTableOutput("table3")
+    )
   )
 )
 
 server <- function (input,output){
   output$table1 <- renderDataTable(
-    datatable(metadata)
+    datatable(metadata, caption = htmltools::tags$caption(
+      style = 'caption-side: bottom;',
+      'table: Metadata'))
   )
   output$table2 <- renderDataTable(
-    datatable(tax_table)
+    datatable(tax_table, caption = htmltools::tags$caption(
+      style = 'caption-side: bottom;',
+      'table: Taxonomy data'))
   )
   output$table3 <- renderDataTable(
-    datatable(count_table)
+    datatable(count_table, caption = htmltools::tags$caption(
+      style = 'caption-side: bottom;',
+      'table: Counts data'))
   )
 }
 
